c8b9e4e409aff4b1c726ee3f197b93041f42746e
Malachitopp <153690692+Malachitopp@users.noreply.github.com>
2026-08-30 18:34:18 +0100

    Add: strobogrammatic-number - submission-2

---
+++ Data Structures & Algorithms/strobogrammatic-number/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isStrobogrammatic(self, num: str) -> bool:
+        rotated_digits = {'0': '0', '1': '1', '8': '8', '6': '9', '9': '6'}
+
+        l = 0
+        r = len(num)- 1
+
+        while l <= r:
+            if num[l] not in rotated_digits or rotated_digits[num[l]] != num[r] :
+                return False
+            l += 1
+            r-= 1
+        
+        return True
+            
+
+
+
+
