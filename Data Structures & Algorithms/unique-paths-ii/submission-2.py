5d5764d5d0fc85a814d385616220cd3f2cc0f852
Malachitopp <153690692+Malachitopp@users.noreply.github.com>
2026-09-23 01:12:12 +0100

    Add: unique-paths-ii - submission-2

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def uniquePathsWithObstacles(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        dp = {(ROWS - 1, COLS - 1): 1 }
+        
+        def dfs(r,c):
+            if r == ROWS or c == COLS  or grid[r][c] :
+                return 0 
+            if (r,c) in dp:
+                return dp[(r,c)] 
+            
+            dp[(r,c)] = dfs(r +1 , c) + dfs(r, c+ 1 ) 
+            return dp[(r,c)]
+
+        return    dfs(0,0)
+    
+
+
+
+
+
+
+
